e7dec7570f75a6201ba0f960baa3f6efb9629139
Umit Yildirim <umity@uia.no>
2023-04-19 15:42:34 +0200

    go.sum

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/uia-worker/is105sem03
+module github.com/yildirimsinop/is105sem03.git
 
 go 1.19
+
+require github.com/uia-worker/is105sem03 v0.0.0-20230416140235-f3824ac768bb
--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -18,7 +18,7 @@ func TestKrypter(t *testing.T) {
 		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
 		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
 		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("dnizmocdd7;484c5: 47 6466d45b94c.")},
-                {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
+        {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
 	}
 
 	for _, tc := range tests {
